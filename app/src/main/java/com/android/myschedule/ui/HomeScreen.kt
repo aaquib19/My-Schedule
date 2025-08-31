a40b134d4cfbe755b4b90d9b13a3b4575461401e
aaquibniaz <aaquibniaz3600@gmail.com>
2025-08-31 09:54:10 +0530

    Added calendar support in homescreen

--- app/src/main/java/com/android/myschedule/ui/HomeScreen.kt
+++ app/src/main/java/com/android/myschedule/ui/HomeScreen.kt
@@ -15,11 +15,15 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardDoubleArrowLeft
+import androidx.compose.material.icons.filled.KeyboardDoubleArrowRight
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarDuration
@@ -32,8 +36,10 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.rememberSwipeToDismissBoxState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
@@ -45,6 +51,8 @@ import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.android.myschedule.data.Task
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -57,6 +65,9 @@ fun HomeScreen(
     val snackbarHostState = remember { SnackbarHostState() }
     val scope = rememberCoroutineScope()
 
+    val formatter = remember { DateTimeFormatter.ofPattern("EEE, dd MMM") }
+    var currentDate by remember { mutableStateOf(LocalDate.now()) }
+
     Scaffold(
         snackbarHost = { SnackbarHost(snackbarHostState) },
         floatingActionButton = {
@@ -68,6 +79,16 @@ fun HomeScreen(
             }
         }
     ) { paddingValues ->
+        Column(
+            modifier = Modifier.padding(paddingValues)
+        ) {
+            DaySwitcherHeader(
+                date = currentDate,
+                onPrevious = {currentDate = currentDate.minusDays(1)},
+                onNext = { currentDate = currentDate.plusDays(1)},
+                formatter = formatter
+            )
+        }
         TaskContent(
             tasks = tasks,
             onEditTask = onEditTask,
@@ -77,6 +98,32 @@ fun HomeScreen(
             modifier = Modifier.padding(paddingValues)
         )
     }
+}
+
+@Composable
+fun DaySwitcherHeader(
+    date: LocalDate,
+    onPrevious: () -> Unit,
+    onNext: () -> Unit,
+    formatter: DateTimeFormatter,
+    modifier: Modifier = Modifier
+) {
+    Row {
+        IconButton(onClick = onPrevious) {Icon(
+            imageVector = Icons.Filled.KeyboardDoubleArrowLeft,
+            contentDescription = "Previous Day"
+        ) }
+        Column {
+            Text(text = date.format(formatter))
+            if(date == LocalDate.now()){
+                Text("Today")}
+        }
+        IconButton(onClick = onPrevious) {Icon(
+            imageVector = Icons.Filled.KeyboardDoubleArrowRight,
+            contentDescription = "Next Day"
+        ) }
+    }
+
 }
 
 @Composable
